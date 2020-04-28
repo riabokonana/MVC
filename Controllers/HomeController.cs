1a635984e7eeb2ea703569deb8f183feedf34c0d
Ana <ryabokon97@ya.ru>
2020-04-28 12:58:40 +0500

    -

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,64 +40,6 @@ namespace FileWorkPril.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        // функция загрузки файла на сервер 
-        [HttpPost]
-        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
-        {
-            if (uploadedFile != null)
-            {
-                // путь к папке Files
-                string path = "/Files/" + uploadedFile.FileName;
-                // сохраняем файл в папку Files в каталоге wwwroot
-                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
-                {
-                    await uploadedFile.CopyToAsync(fileStream);
-                }
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        // функция получения списка файлов
-        public List<string> getFiles()
-        {
-            List<string> files = new List<string>();
-            string path = "/Files/";
-
-            DirectoryInfo source = new DirectoryInfo(_appEnvironment.WebRootPath + path);
-
-            foreach (FileInfo fi in source.GetFiles())
-            {
-                files.Add(fi.Name);
-            }
-
-            return files;
-        }
-
-        // функция скачивания файла
-        [HttpPost]
-        public VirtualFileResult GetVirtualFile(string filename)
-        {
-            var filepath = Path.Combine("~/Files", filename);
-            return File(filepath, "text/plain", filename);
-        }
-
-        // функция удаления файла
-        [HttpPost]
-        public IActionResult RemoveFile(string filename)
-        {
-            if (filename != null)
-            {
-                // путь к папке Files
-                string path = "/Files/" + filename;
-
-                if (System.IO.File.Exists(_appEnvironment.WebRootPath + path))
-                {
-                    System.IO.File.Delete(_appEnvironment.WebRootPath + path);
-                }
-            }
-
-            return RedirectToAction("Index");
-        }
+        
     }
 }
