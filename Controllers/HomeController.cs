9bc5b4a9193f4c3516bd8977351d1d2160aead9b
Ana <ryabokon97@ya.ru>
2020-04-28 12:59:43 +0500

    Функция загрузки файла на сервер

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,24 @@ namespace FileWorkPril.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        // функция загрузки файла на сервер 
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
         
     }
 }
